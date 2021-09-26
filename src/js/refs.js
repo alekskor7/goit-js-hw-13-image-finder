0c02e17301f9e42a0c25f8c5d79efa3cb2b28f85
Oleksandr Korniiets <alekskor7@gmail.com>
2021-09-27 00:17:31 +0300

    hw13

---
+++ src/js/refs.js
@@ -0,0 +1,10 @@
+const refs = {
+    gallery : document.querySelector('.gallery'),
+    form : document.querySelector('.search-form'),
+    input: document.querySelector('input'),
+    KEY : '23523272-7e7e55898992c75b8603edf48',
+    pageNumber: 1,
+    upBtn: document.querySelector('.to-top'),
+ }
+
+ export default refs
